f05e8ca141900bf2301f3edf1277fe55949b6552
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-03 14:28:09 -0300

    empezando con la clase 01 - repaso

---
+++ clase-01/01.repaso-py/main.py
@@ -0,0 +1,39 @@
+print("Hola mundo!")
+
+""" 
+! Python es un lenguaje:
+
+* Alto nivel.
+* Interpretado.
+* Dinámicamente tipado.
+* Sintaxis sencilla.
+* Multiproposito -> Inicialmente se usaba para scripting
+* Gran comunidad.
+
+! Nos permite hacer
+
+* Desarrollo Web
+* Ciencia de datos
+* Scripting (Administración de sistema)
+* Backend
+* Cyber Seguridad
+* Video Juegos
+* Automatizaciones
+* Inteligencia artificial
+* App de escritorio
+"""
+
+""" Print -> Permite imprimir en terminal/consola """
+
+print("Hola tarolas")
+
+print(10 + 20)
+
+nombre = "Maxi"
+version = 3
+
+print(nombre)
+print(version)
+
+""" IMPORTANTE: El programa al ser interpretado. Las instrucciones se ejecutan de arriba para abajo y de izquierda a derechas.  """
+
